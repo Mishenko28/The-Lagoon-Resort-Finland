5ba000a14d19d374487d7bdf46a4016ce8192750
John Thomas <johnthomasalog@gmail.com>
2025-03-25 14:51:04 +0800

    Add search functionality to Pending bookings; enhance UI with improved input styling and dynamic filtering

--- frontend/adminpage/src/pages/Dashboard/Booking.jsx
+++ frontend/adminpage/src/pages/Dashboard/Booking.jsx
@@ -26,7 +26,6 @@ export default function Booking() {
             </div>
             {page === "pending" && <Pending />}
             {page === "confirmed" && <Confirmed />}
-
         </div>
     )
 }
--- frontend/adminpage/src/pages/Dashboard/Booking/Pending.jsx
+++ frontend/adminpage/src/pages/Dashboard/Booking/Pending.jsx
@@ -18,6 +18,8 @@ export default function Pending() {
 
     const [openedNote, setOpenedNote] = useState("")
 
+    const [searchInput, setSearchInput] = useState("")
+
     useEffect(() => {
         fetchBooks()
         socket.connect()
@@ -45,80 +47,95 @@ export default function Pending() {
     if (isLoading) return <Loader2 />
 
     return (
-        <div className="book">
-            <table>
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Email</th>
-                        <th>Guest</th>
-                        <th>Contact Number</th>
-                        <th>Timeframe</th>
-                        <th>Room Type</th>
-                        <th>Note</th>
-                        <th>Down Payment</th>
-                        <th>Total</th>
-                        <th>Submitted since</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <AnimatePresence mode="sync">
-                        {books.map((book, i) => (
-                            <motion.tr
-                                layout
-                                initial={{ opacity: 0.5 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.3 }}
-                                key={book._id}
-                            >
-                                <td>{i + 1}</td>
-                                <td>{book.user.email}</td>
-                                <td>
-                                    {book.user.name}
-                                    <br />
-                                    {book.user.sex}, {book.user.age}yrs old
-                                </td>
-                                <td>{book.user.contact}</td>
-                                <td>
-                                    {format(book.from, 'LLL d' + (new Date(book.from).getFullYear() === new Date(book.to).getFullYear() ? '' : ', yyyy'))} - {format(book.to, (new Date(book.from).getMonth() === new Date(book.to).getMonth() ? '' : 'LLL ') + 'd, yyyy')}
-                                    <br />
-                                    {formatDistance(book.from, book.to)}
-                                </td>
-                                <td>
-                                    {book.room.map((room, i) => (
-                                        <div key={i}>
-                                            {room.roomType}
-                                            {room.addedPerson > 0 &&
-                                                <span>(+{room.addedPerson} < i className="fa-solid fa-person" />)</span>
-                                            }
-                                        </div>
-                                    ))}
-                                </td>
-                                <td>{book.note && <i onClick={() => setOpenedNote(book.note)} className="fa-solid fa-envelope" />}</td>
-                                <td>₱{book.deposit}</td>
-                                <td>₱{book.total}</td>
-                                <td>{formatDistanceToNow(book.createdAt, { addSuffix: 1 })}</td>
-                                <td>
-                                    <div className="bttns">
-                                        <button onClick={() => setToConfirm(book)} className="green">Confirm</button>
-                                        <button onClick={() => setToCancel(book)} className="red">Cancel</button>
-                                    </div>
-                                </td>
-                            </motion.tr>
-                        ))}
-                    </AnimatePresence>
-                    {books.length === 0 && (
+        <>
+            <div className="book-header">
+                <input value={searchInput} onChange={e => !(searchInput.length === 0 && e.target.value === " ") && setSearchInput(e.target.value)} type="text" placeholder="search for email or guest name" />
+            </div>
+            <div className="book">
+                <table>
+                    <thead>
                         <tr>
-                            <td colSpan="11" className="center">No pending bookings.</td>
+                            <th>No.</th>
+                            <th>Email</th>
+                            <th>Guest</th>
+                            <th>Contact Number</th>
+                            <th>Timeframe</th>
+                            <th>Room Type</th>
+                            <th>Note</th>
+                            <th>Down Payment</th>
+                            <th>Total</th>
+                            <th>Submitted since</th>
+                            <th>Actions</th>
                         </tr>
-                    )}
-                </tbody>
-            </table>
-            {toCancel && <CancelBook setBooks={setBooks} setToCancel={setToCancel} toCancel={toCancel} />}
-            {toConfirm && <ConfirmBook setBooks={setBooks} setToConfirm={setToConfirm} toConfirm={toConfirm} />}
-            {openedNote && <Note openedNote={openedNote} setOpenedNote={setOpenedNote} />}
-        </div>
+                    </thead>
+                    <tbody>
+                        <AnimatePresence mode="sync">
+                            {books.filter(book => new RegExp(searchInput, 'i').test(book.user.email) || new RegExp(searchInput, 'i').test(book.user.name)).map((book, i) => (
+                                <motion.tr
+                                    layout
+                                    initial={{ opacity: 0.5 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    transition={{ duration: 0.3 }}
+                                    key={book._id}
+                                >
+                                    <td>{i + 1}</td>
+                                    <td>
+                                        {book.user.email.split(new RegExp(`(${searchInput})`, 'gi')).map((part, i) => (
+                                            <span key={i} style={part.toLowerCase() === searchInput.toLowerCase() ? { backgroundColor: "var(--primary)", color: "#fff" } : null}>
+                                                {part}
+                                            </span>
+                                        ))}
+                                    </td>
+                                    <td>
+                                        {book.user.name.split(new RegExp(`(${searchInput})`, 'gi')).map((part, i) => (
+                                            <span key={i} style={part.toLowerCase() === searchInput.toLowerCase() ? { backgroundColor: "var(--primary)", color: "#fff" } : null}>
+                                                {part}
+                                            </span>
+                                        ))}
+                                        <br />
+                                        {book.user.sex}, {book.user.age}yrs old
+                                    </td>
+                                    <td>{book.user.contact}</td>
+                                    <td>
+                                        {format(book.from, 'LLL d' + (new Date(book.from).getFullYear() === new Date(book.to).getFullYear() ? '' : ', yyyy'))} - {format(book.to, (new Date(book.from).getMonth() === new Date(book.to).getMonth() ? '' : 'LLL ') + 'd, yyyy')}
+                                        <br />
+                                        {formatDistance(book.from, book.to)}
+                                    </td>
+                                    <td>
+                                        {book.room.map((room, i) => (
+                                            <div key={i}>
+                                                {room.roomType}
+                                                {room.addedPerson > 0 &&
+                                                    <span>(+{room.addedPerson} < i className="fa-solid fa-person" />)</span>
+                                                }
+                                            </div>
+                                        ))}
+                                    </td>
+                                    <td>{book.note && <i onClick={() => setOpenedNote(book.note)} className="fa-solid fa-envelope" />}</td>
+                                    <td>₱{book.deposit}</td>
+                                    <td>₱{book.total}</td>
+                                    <td>{formatDistanceToNow(book.createdAt, { addSuffix: 1 })}</td>
+                                    <td>
+                                        <div className="bttns">
+                                            <button onClick={() => setToConfirm(book)} className="green">Confirm</button>
+                                            <button onClick={() => setToCancel(book)} className="red">Cancel</button>
+                                        </div>
+                                    </td>
+                                </motion.tr>
+                            ))}
+                        </AnimatePresence>
+                        {books.length === 0 && (
+                            <tr>
+                                <td colSpan="11" className="center">No bookings.</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+                {toCancel && <CancelBook setBooks={setBooks} setToCancel={setToCancel} toCancel={toCancel} />}
+                {toConfirm && <ConfirmBook setBooks={setBooks} setToConfirm={setToConfirm} toConfirm={toConfirm} />}
+                {openedNote && <Note openedNote={openedNote} setOpenedNote={setOpenedNote} />}
+            </div >
+        </>
     )
 }
